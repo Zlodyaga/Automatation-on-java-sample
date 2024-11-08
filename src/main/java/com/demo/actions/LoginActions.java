b11ecfbe343b227bc83cc907c8a159f11b9e35e1
Bohomazov Dmytro <dimonbog2004@gmail.com>
2024-11-08 13:46:56 +0200

    Added testing of 2 features
    Advanced search past days
    Get last transaction

--- src/main/java/com/demo/actions/LoginActions.java
+++ src/main/java/com/demo/actions/LoginActions.java
@@ -1,19 +1,22 @@
 package com.demo.actions;
 
 import com.demo.pages.Pages;
+import io.qameta.allure.Step;
 import org.testng.Assert;
 
 public class LoginActions {
 
+    @Step("Login with enrolled client's data")
     public void doLogin(String userName, String password) {
         Pages.loginPage().waitForLoginForm();
         Pages.loginPage().setUsername(userName);
         Pages.loginPage().setPassword(password);
         Pages.loginPage().clickLoginButton();
         Pages.navigationPage().waitForUserMenu();
-        Assert.assertEquals(Pages.navigationPage().getHeaderText(), "some text");
+        Assert.assertEquals(Pages.navigationPage().getHeaderText(), "Adamtms Jacksonniqjj");
     }
 
+    @Step("Logout")
     public void doLogout() {
         Pages.navigationPage().hoverOverUserMenu();
         Pages.navigationPage().clickLogoutButton();
